02dbf2ccf0f2438d79f2046c5cc6705a6a119877
Nick Allen <nick.allen.1093@gmail.com>
2015-11-12 13:32:24 +0000

    Update Calc.c

--- Calc.c
+++ Calc.c
@@ -12,44 +12,45 @@ int power (int base, int exp);
 int converter (char *number);
 int Calc (str);
 
-int Calc (str) {
+int Calc (char* str) {
  
 //char str[128]; //can remove as its input
 char *ptr; 
-strcpy (str, "10D7"); // input string to test
+//strcpy (str, "10D7"); // input string to test
 int w = strlen(str);
 double divtotal = 0;
 int total, result, result1 = 0;
-for (int i = 0; i < w; i++){
-		
-	if (str[i] == 'A'){ //Add
-		strtok_r (str, "A", &ptr);
-		result = converter(str);
-		result1 = converter(ptr);
-		total = result + result1;
-		return total;}
-	else if (str[i] == 'B'){ //Subtract
-		strtok_r (str, "B", &ptr);
-		result = converter(str);
-		result1 = converter(ptr);
-		total = result - result1;
-		return total;}
-	else if (str[i] == '*'){ //Times
-		strtok_r (str, "*", &ptr);
-		result = converter(str);
-		result1 = converter(ptr);
-		total = result * result1;
-		return total;} 
-	else if (str[i] == 'D'){ //Divide
-		strtok_r (str, "D", &ptr);
-		result = converter(str);
-		result1 = converter(ptr);
-		if (result1 == 0){
-			return "Error";}
-		else{		
-			divtotal = (double)result / result1;
-			return divtotal;}}
-	else{continue;}
+int i;
+for (i = 0; i < w; i++){
+        
+    if (str[i] == 'A'){ //Add
+        strtok_r (str, "A", &ptr);
+        result = converter(str);
+        result1 = converter(ptr);
+        total = result + result1;
+        return total;}
+    else if (str[i] == 'B'){ //Subtract
+        strtok_r (str, "B", &ptr);
+        result = converter(str);
+        result1 = converter(ptr);
+        total = result - result1;
+        return total;}
+    else if (str[i] == '*'){ //Times
+        strtok_r (str, "*", &ptr);
+        result = converter(str);
+        result1 = converter(ptr);
+        total = result * result1;
+        return total;} 
+    else if (str[i] == 'D'){ //Divide
+        strtok_r (str, "D", &ptr);
+        result = converter(str);
+        result1 = converter(ptr);
+        if (result1 == 0){
+            return -1;}
+        else{       
+            divtotal = (double)result / result1;
+            return divtotal;}}
+    else{continue;}
 }
 
 return 0;
@@ -69,11 +70,12 @@ int power(int base, int exp) {
 
 
 int converter (char *number){
-	int result = 0;
-	int count = (strlen(number)-1);
-	for (int x = 0; x < strlen(number); x++){		
-	int y = number[x] -48; // turns ascii to number
-	result += y * power(10, count);			
-	count--;}
-	return result;
+    int result = 0;
+    int count = (strlen(number)-1);
+    int x;
+    for (x = 0; x < strlen(number); x++){       
+    int y = number[x] -48; // turns ascii to number
+    result += y * power(10, count);         
+    count--;}
+    return result;
 }
